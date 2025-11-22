6fba8249949e3ab86db9eec1db2eb5eb762dc2fa
Kai Doo <endmsgur@gmail.com>
2025-11-22 13:55:55 -0500

    fix: apply ESLint rules to .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,24 +5,24 @@ module.exports = {
   },
 
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "airbnb-base",
-    "airbnb-typescript/base"
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb-base',
+    'airbnb-typescript/base',
   ],
 
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
 
   parserOptions: {
-    project: "./tsconfig.json",
+    project: './tsconfig.json',
     tsconfigRootDir: __dirname,
   },
 
-  plugins: ["@typescript-eslint"],
+  plugins: ['@typescript-eslint'],
 
   rules: {
-    "no-console": "warn",
-    "import/prefer-default-export": "off",
-    "import/extensions": "off",
+    'no-console': 'warn',
+    'import/prefer-default-export': 'off',
+    'import/extensions': 'off',
   },
 };
