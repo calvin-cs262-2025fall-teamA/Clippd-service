e9fcd62ac84edbd91c4c1738cedd036a502c35e7
Isaac Hur <ish7@calvin.edu>
2025-11-22 02:30:21 -0500

    add .github/workflows/eslint.yml and edit eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,31 @@
 import js from "@eslint/js";
 import globals from "globals";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
+import { configs as tseslintConfigs } from "typescript-eslint";
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
-  tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-]);
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    languageOptions: {
+      globals: {
+        ...globals.node  // browser 제거, node만
+      }
+    },
+    rules: {
+      // Airbnb Style Guide rules
+      "indent": ["error", 2],
+      "quotes": ["error", "single"],
+      "semi": ["error", "always"],
+      "comma-dangle": ["error", "always-multiline"],
+      "no-unused-vars": ["warn", { 
+        "argsIgnorePattern": "^_",
+        "varsIgnorePattern": "^_"
+      }],
+      "no-console": "off",  // 서버는 console.log 허용
+      "eqeqeq": ["error", "always"],
+      "curly": ["error", "all"],
+      "brace-style": ["error", "1tbs"],
+    }
+  },
+  js.configs.recommended,
+  ...tseslintConfigs.recommended,
+];
