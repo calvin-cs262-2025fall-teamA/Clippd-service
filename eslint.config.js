75fe886ee1d44f5f7872c903926af0c378af8fdc
tet2calv <tet2@calvin.edu>
2025-11-22 22:16:15 -0500

    setup eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,30 +1,30 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { configs as tseslintConfigs } from "typescript-eslint";
+import js from '@eslint/js';
+import globals from 'globals';
+import { configs as tseslintConfigs } from 'typescript-eslint';
 
 export default [
   {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
     languageOptions: {
       globals: {
-        ...globals.node  // browser 제거, node만
-      }
+        ...globals.node,  // browser 제거, node만
+      },
     },
     rules: {
       // Airbnb Style Guide rules
-      "indent": ["error", 2],
-      "quotes": ["error", "single"],
-      "semi": ["error", "always"],
-      "comma-dangle": ["error", "always-multiline"],
-      "no-unused-vars": ["warn", { 
-        "argsIgnorePattern": "^_",
-        "varsIgnorePattern": "^_"
+      'indent': ['error', 2],
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'always'],
+      'comma-dangle': ['error', 'always-multiline'],
+      'no-unused-vars': ['warn', { 
+        'argsIgnorePattern': '^_',
+        'varsIgnorePattern': '^_',
       }],
-      "no-console": "off",  // 서버는 console.log 허용
-      "eqeqeq": ["error", "always"],
-      "curly": ["error", "all"],
-      "brace-style": ["error", "1tbs"],
-    }
+      'no-console': 'off',  // 서버는 console.log 허용
+      'eqeqeq': ['error', 'always'],
+      'curly': ['error', 'all'],
+      'brace-style': ['error', '1tbs'],
+    },
   },
   js.configs.recommended,
   ...tseslintConfigs.recommended,
